7a42372bbf32cc28866e6a784e55e9b0c1c3af79
PrudnikovVetal <vpprudnikov@gmail.com>
2022-09-29 12:39:44 +0300

    final committe

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require('path')
-const HTMLPlugin = require('html-webpack-plugin')
+const path = require("path")
+const HTMLPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
   },
   devServer: {
-    port: 4200
+    port: 4200,
   },
   plugins: [
     new HTMLPlugin({
-      template: './src/index.html'
-    })
-  ]
+      template: "./src/index.html",
+    }),
+  ],
 }
